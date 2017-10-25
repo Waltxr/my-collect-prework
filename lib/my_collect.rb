7cf370aedad07ec28a321326b0ff347b5cf9e060
Adam Walter <adam.iverson.walter@gmail.com>
2017-10-25 20:06:51 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,11 +2,8 @@ def my_collect(collection)
   i = 0
 
   while i < collection.length
-    collection[i].collect do
-      yield
-    end
+    yield collection[i]
     i += 1
   end
-
   return collection
 end
